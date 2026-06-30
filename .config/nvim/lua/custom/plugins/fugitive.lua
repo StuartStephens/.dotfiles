700242d64989e910f8ed169dc45240eac307180e
Stuart <contact@stuartstephens.com>
2026-06-30 14:58:51 -0400

    improve keybinds

--- .config/nvim/lua/custom/plugins/fugitive.lua
+++ .config/nvim/lua/custom/plugins/fugitive.lua
@@ -1,2 +1,3 @@
 vim.pack.add { 'https://github.com/tpope/vim-fugitive' }
 vim.keymap.set('n', '<leader>G', '<cmd>vertical G<cr>', { desc = '[G]it Fugitive' })
+vim.keymap.set('n', '<leader>g', '<cmd>belowright split | G<cr>', { desc = '[g]it Fugitive horizontal' })
